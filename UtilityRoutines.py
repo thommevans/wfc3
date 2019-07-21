0ade373d4f89c3899f6b675b6c8fd19585250f4f
tomevans <tmevans@mit.edu>
2019-07-21 13:55:54 -0400

    corrected typo in previous documentation for rvFunc() and r0Func()

--- UtilityRoutines.py
+++ UtilityRoutines.py
@@ -9,7 +9,7 @@ import numexpr
 def rvFunc( t, a1, a2 ):
     """
     Within the context of the detector charge-trapping model,
-    a reasonable physical constraint is a1>0 and a2<0. These
+    a reasonable physical constraint is a1<0 and a2>0. These
     restrictions are not applied within this routine, but can 
     be easily be built into whatever optimization metric is used 
     for determining the unknown parameters of the ramp model.
@@ -19,7 +19,7 @@ def rvFunc( t, a1, a2 ):
 def r0Func( torb, rvt, a3, a4, a5 ):
     """
     Within the context of the detector charge-trapping model, 
-    a reasonable physical constraint is a3>0, a4<0, and 
+    a reasonable physical constraint is a3<0, a4>0, and 
     a5<torb.min(). These restrictions are not applied within 
     this routine, but can be easily be built into whatever 
     optimization metric is used for determining the unknown 
